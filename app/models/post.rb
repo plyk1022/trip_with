82766590eb9fd1679ed29dfcd77f203156770268
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-10-04 16:20:14 +0000

    いいね機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,11 @@ class Post < ApplicationRecord
   has_many :prefecture_relations, dependent: :destroy
   has_many :prefectures, through: :prefecture_relations
   accepts_nested_attributes_for :prefecture_relations, allow_destroy: true
+
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
