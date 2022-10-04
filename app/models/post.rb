f5d3bc3fe36db815774d87dcb3483cdd88f32fc8
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-10-04 03:23:03 +0000

    投稿画面のレイアウト調整及び閲覧・編集画面作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
-  
+
   belongs_to :user
-  
+
   has_many :schedules, dependent: :destroy
-  accepts_nested_attributes_for :schedules
-  
+  accepts_nested_attributes_for :schedules, allow_destroy: true
+
   has_many :prefecture_relations, dependent: :destroy
   has_many :prefectures, through: :prefecture_relations
-  accepts_nested_attributes_for :prefecture_relations
+  accepts_nested_attributes_for :prefecture_relations, allow_destroy: true
 end
