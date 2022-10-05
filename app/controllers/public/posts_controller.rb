ccad04aa58366e6bed49c4e13c33c438222b2934
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-10-05 04:33:13 +0000

    写真投稿機能及び投稿詳細レイアウト調整

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,30 +1,30 @@
 class Public::PostsController < ApplicationController
-  
+
   def new
     @post = Post.new
     @post.prefecture_relations.build
-    
+
   end
-  
+
   def form
     @post = Post.new(post_params)
-    
+
     @trip_days = (@post.end_date - @post.start_date).to_i + 1
-    
+
     date = @post.start_date
-    
+
     @trip_days.times do
       schedule = @post.schedules.build
       3.times{ schedule.spots.build }
-      
-      
+
+
       schedule.date = date
       date = date + 1
     end
-    
-    
+
+
   end
-  
+
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
@@ -39,24 +39,24 @@ class Public::PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def edit
     @post = Post.find(params[:id])
-    
+
   end
-  
+
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to post_path(@post.id)
   end
-  
-  
+
+
   private
   def post_params
     params.require(:post).permit(:title, :body, :start_date, :end_date,
                                   prefecture_relations_attributes:[:id, :prefecture_id, :_destroy],
-                                  schedules_attributes:[:id, :date, :_destroy, 
-                                  spots_attributes:[:id, :name, :comment, :arriving_time, :leaving_time, :image, :_destroy]])
+                                  schedules_attributes:[:id, :date, :_destroy,
+                                  spots_attributes:[:id, :name, :comment, :arriving_time, :leaving_time, :spot_image, :_destroy]])
   end
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,6 +1,14 @@
 class Spot < ApplicationRecord
-  
+
   belongs_to :schedule
-  has_one_attached :image
-  
+  has_one_attached :spot_image
+
+  def get_spot_image(width, height)
+    unless spot_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      spot_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    spot_image.variant(resize_to_fill: [width, height]).processed
+  end
+
 end
