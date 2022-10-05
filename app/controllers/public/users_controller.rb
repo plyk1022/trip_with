fab51bac199cebaa99dd68450c099f15ffc042f3
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-10-05 10:55:34 +0000

    ユーザー編集機能追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,7 +1,21 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+    
 end
