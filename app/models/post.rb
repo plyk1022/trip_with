ffc7181b7470bc1c4f162c453840c6c29d065705
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-10-06 16:58:15 +0000

    お気に入り機能　不具合修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ApplicationRecord
   accepts_nested_attributes_for :prefecture_relations, allow_destroy: true
 
   has_many :favorites, dependent: :destroy
-   has_many :comments, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
