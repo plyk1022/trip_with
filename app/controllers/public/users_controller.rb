364a5ec49eb59bc75a1edda23f5ee928d5048504
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-10-06 03:53:22 +0000

    ゲストログイン機能追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,6 @@
 class Public::UsersController < ApplicationController
+  before_action :ensure_guest_user, only: [:edit]
+  
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
@@ -14,8 +16,17 @@ class Public::UsersController < ApplicationController
     redirect_to user_path(@user)
   end
   
+  private
+  
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
+  
+   def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "ゲストユーザー"
+      redirect_to posts_path , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end  
     
 end
