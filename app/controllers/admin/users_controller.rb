8192370808cf1e9bbd3323d0e9d16abae8bf7045
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-10-10 06:22:48 +0000

    ユーザー退会機能追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,5 +12,19 @@ class Admin::UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_user_path(@user)
+  end
+  
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:is_deleted)
+  end
 
 end
