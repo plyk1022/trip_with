47f9f086a837364d631cf4ca39c3f780b973802e
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-10-18 05:36:40 +0000

    退会処理追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Public::SessionsController < Devise::SessionsController
-  
+  before_action :user_state, only: [:create]
+
   def after_sign_in_path_for(resource)
     posts_path
   end
@@ -8,12 +9,23 @@ class Public::SessionsController < Devise::SessionsController
     new_user_session_path
   end
 
- 
+
+  protected
+
+  def user_state
+    @user = User.find_by(email: params[:user][:email])
+    return if !@user
+
+    if @user.valid_password?(params[:user][:password]) && @user.is_deleted
+      redirect_to new_user_registration_path
+    end
+  end
+
+
 
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to user_path(user), notice: 'guestuserでログインしました。'
   end
 
 end
