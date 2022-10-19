ac3cde438e3a756f57bfa35e9274d28ae06a0a9c
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-10-19 06:20:27 +0000

    フラッシュメッセージ追加及びデザイン修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -13,6 +13,7 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
+    flash[:notice] = 'ゲストユーザーでログインしました。'
     redirect_to user_path(user)
   end
 
